6b221db3a1b6dd05cd295c7012567beaca8ccc7d
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-12-02 22:43:31 +0100

    fix mistake - not extending Repository interface

--- backend/post-service/src/main/java/com/speakapp/postservice/repositories/PostReactionRepository.java
+++ backend/post-service/src/main/java/com/speakapp/postservice/repositories/PostReactionRepository.java
@@ -2,12 +2,14 @@ package com.speakapp.postservice.repositories;
 
 import com.speakapp.postservice.entities.Post;
 import com.speakapp.postservice.entities.PostReaction;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface PostReactionRepository {
+public interface PostReactionRepository extends JpaRepository<PostReaction, UUID> {
 
     List<PostReaction> findAllByPost(Post post);
 }
