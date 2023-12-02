cbdf1c8c90af58aa1f4128594cce80d3dba6d057
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-12-02 14:18:28 +0100

    Define mapper for creating post and getting post functionality

---
+++ backend/post-service/src/main/java/com/speakapp/postservice/mappers/PostMapper.java
@@ -0,0 +1,25 @@
+package com.speakapp.postservice.mappers;
+
+import com.speakapp.postservice.dtos.*;
+import com.speakapp.postservice.entities.Post;
+import com.speakapp.postservice.entities.ReactionType;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Mapper
+public interface PostMapper {
+
+    @Mapping(target = "isDeleted", constant = "true")
+    @Mapping(target = "postId", ignore = true)
+    public Post toEntity(PostCreateDTO postCreateDTO, UUID userId);
+
+    public PostGetDTO toGetDTO(Post post,
+                               UserGetDTO author,
+                               Set<CommentGetDTO> comments,
+                               ReactionsGetDTO reactions,
+                               ReactionType currentUserReaction);
+
+}
