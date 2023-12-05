27d475625be8ae938913b9242e18769dd514d34c
szlazakm <01169634@pw.edu.pl>
2023-12-05 12:23:38 +0100

    DTO's for AppUser Update and Create. DTO for Photo update (profile or background)

---
+++ backend/user-service/src/main/java/com/speakapp/userservice/dtos/PhotoUpdateDTO.java
@@ -0,0 +1,12 @@
+package com.speakapp.userservice.dtos;
+
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+@Value
+@Builder
+@Jacksonized
+public class PhotoUpdateDTO {
+    String photoUrl;
+}
