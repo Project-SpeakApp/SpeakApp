973140396d57afbda6c473bd6ce224034b6adaad
szlazakm <01169634@pw.edu.pl>
2023-12-07 22:36:11 +0100

    Solved problem with mappin Instant, refactored internal user service

--- backend/user-service/src/main/java/com/speakapp/userservice/services/InternalUserService.java
+++ backend/user-service/src/main/java/com/speakapp/userservice/services/InternalUserService.java
@@ -1,15 +1,11 @@
 package com.speakapp.userservice.services;
 
 import com.speakapp.userservice.dtos.AppUserPreviewDTO;
-import com.speakapp.userservice.entities.AppUser;
 import com.speakapp.userservice.mappers.AppUserMapper;
 import com.speakapp.userservice.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,17 +15,10 @@ public class InternalUserService {
     private final UserRepository userRepository;
     private final AppUserMapper appUserMapper;
     public List<AppUserPreviewDTO> getUsersPreviews(List<UUID> userIds) {
-
-        List<AppUserPreviewDTO> userPreviewDTOS = new ArrayList<>(userIds.size());
-        for(UUID userId : userIds) {
-            Optional<AppUser> appUser = userRepository.findById(userId);
-
-            AppUserPreviewDTO userPreviewDTO = appUser
-                    .map(appUserMapper::appUserPreviewDtoFromAppUser)
-                    .orElse(AppUserPreviewDTO.empty(userId));
-
-            userPreviewDTOS.add(userPreviewDTO);
-        }
-        return userPreviewDTOS;
+        return userIds.stream()
+                .map(userId -> userRepository.findById(userId)
+                        .map(appUserMapper::toAppUserPreviewDto)
+                        .orElse(AppUserPreviewDTO.empty(userId)))
+                .toList();
     }
 }
