fe606329f4cc09bd1bcb9dcb1bf1329e5ba5a23b
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-12-07 17:22:16 +0100

    add data seeder with relationships and modify names of some entities

--- backend/user-service/src/main/resources/data-postgres.sql
+++ backend/user-service/src/main/resources/data-postgres.sql
@@ -1,13 +1,18 @@
--- Insert data into app_user table
-INSERT INTO public.app_user (date_of_birth, last_online, user_id, first_name, last_name, about, email, password)
-VALUES
-    ('1990-01-01'::date, '2023-01-01 12:00:00'::timestamp, '1f8a6280-12c4-11ec-82a8-0242ac130003'::uuid, 'John', 'Doe', 'Lorem ipsum dolor sit amet.', 'john.doe@example.com', 'password123'),
-    ('1985-05-15'::date, '2023-02-01 12:00:00'::timestamp, '6c84fb92-12c4-11ec-82a8-0242ac130003'::uuid, 'Alice', 'Johnson', 'Consectetur adipiscing elit.', 'alice.j@example.com', 'pass456'),
-    ('1998-07-20'::date, '2023-03-01 12:00:00'::timestamp, '6c84fb95-12c4-11ec-82a8-0242ac130003'::uuid, 'Bob', 'Smith', 'Vivamus et justo nec purus.', 'bob.smith@example.com', 'secure789');
+DO
+$$
+    DECLARE
+        user_id_1         uuid      := '6c84fb95-12c4-11ec-82a8-0242ac130003'::uuid;
+        user_id_2         uuid      := '6c84fb96-12c4-11ec-82a8-0242ac130003'::uuid;
+        user_id_3         uuid      := '6c84fb97-12c4-11ec-82a8-0242ac130003'::uuid;
+    BEGIN
+        INSERT INTO app_user (user_id, first_name, last_name, email, date_of_birth, last_online, about)
+        VALUES (user_id_1, 'John', 'Doe', 'johndoe@gmail.com', '1990-01-01'::date, '2023-01-01 12:00:00'::timestamp, 'Lorem ipsum dolor sit amet.'),
+                (user_id_2, 'Alice', 'Johnson', 'alicejohnson@gmail.com', '1985-05-15'::date, '2023-02-01 12:00:00'::timestamp, 'Consectetur adipiscing elit.'),
+                (user_id_3, 'Christopher', 'Bear', 'christopherbear@gmail.com', '1998-07-20'::date, '2023-03-01 12:00:00'::timestamp, 'Vivamus et justo nec purus.');
 
--- Insert data into user_friend table
-INSERT INTO public.user_friend (created_at, addressee_id, friend_id, requester_id, status)
-VALUES
-    ('2023-01-10 12:00:00'::timestamp, '1f8a6280-12c4-11ec-82a8-0242ac130003'::uuid, '6c84fb92-12c4-11ec-82a8-0242ac130003'::uuid, '6c84fb95-12c4-11ec-82a8-0242ac130003'::uuid, 'FRIEND'),
-    ('2023-02-15 12:00:00'::timestamp, '6c84fb95-12c4-11ec-82a8-0242ac130003'::uuid, '1f8a6280-12c4-11ec-82a8-0242ac130003'::uuid, '6c84fb92-12c4-11ec-82a8-0242ac130003'::uuid, 'FRIEND'),
-    ('2023-03-20 12:00:00'::timestamp, '6c84fb92-12c4-11ec-82a8-0242ac130003'::uuid, '6c84fb95-12c4-11ec-82a8-0242ac130003'::uuid, '1f8a6280-12c4-11ec-82a8-0242ac130003'::uuid, 'REQUEST');
+        INSERT INTO user_friend (requester_id, addressee_id, friend_id, status, created_at)
+        VALUES (user_id_1, user_id_2, '6c84fb98-12c4-11ec-82a8-0242ac130003'::uuid, 'FRIEND', '2023-01-10 12:00:00'::timestamp),
+               (user_id_2, user_id_1, '6c84fb99-12c4-11ec-82a8-0242ac130003', 'FRIEND', '2023-02-15 12:00:00'::timestamp),
+               (user_id_3, user_id_1, '6c84fc91-12c4-11ec-82a8-0242ac130003', 'REQUEST', '2023-03-20 12:00:00'::timestamp);
+    END
+$$;
