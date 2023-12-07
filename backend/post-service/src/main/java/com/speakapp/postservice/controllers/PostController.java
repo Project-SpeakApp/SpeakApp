4dac169c6d66c23b7bf782be38e4ea62a8462c91
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-12-07 09:28:16 +0100

    add logger for debug purposes

--- backend/post-service/src/main/java/com/speakapp/postservice/controllers/PostController.java
+++ backend/post-service/src/main/java/com/speakapp/postservice/controllers/PostController.java
@@ -4,6 +4,7 @@ import com.speakapp.postservice.dtos.PostCreateDTO;
 import com.speakapp.postservice.dtos.PostGetDTO;
 import com.speakapp.postservice.services.PostService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/posts")
 @RequiredArgsConstructor
+@Slf4j
 public class PostController {
 
     private final PostService postService;
@@ -19,6 +21,7 @@ public class PostController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PostGetDTO createPost(@RequestBody PostCreateDTO postCreateDTO, @RequestHeader("UserId") UUID userId) {
+        log.info("Creating post for user with id = {} and post body = {}", userId, postCreateDTO);
         return postService.createPost(postCreateDTO, userId);
     }
 
--- backend/user-service/src/main/java/com/speakapp/userservice/controllers/UserController.java
+++ backend/user-service/src/main/java/com/speakapp/userservice/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.speakapp.userservice.dtos.AppUserDTO;
 import com.speakapp.userservice.exceptions.UserNotFoundException;
 import com.speakapp.userservice.services.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.UUID;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -20,6 +22,7 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     public AppUserDTO getUserById(@PathVariable(name = "userId") UUID userId)
             throws UserNotFoundException {
+        log.info("Getting user info of user with id = {}", userId);
         return userService.getUser(userId);
     }
 }
