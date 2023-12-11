c51cbde29bc49e6280f5a50c78ec9cc13dcc5338
Karol <socer404@gmail.com>
2023-12-11 23:36:00 +0100

    Setup Eureka Server Discovery

--- backend/discovery-service/src/main/java/com/speakapp/discoveryservice/DiscoveryServiceApplication.java
+++ backend/discovery-service/src/main/java/com/speakapp/discoveryservice/DiscoveryServiceApplication.java
@@ -1,8 +1,15 @@
 package com.speakapp.discoveryservice;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
 
 }
