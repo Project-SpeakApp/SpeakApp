30a7b9ff899911a0b18d998e60166a7dee924ffd
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-12-11 19:35:09 +0100

    profile navigation fix and not found page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/home-page/home-page.component';
 import { UserProfilePageComponent } from './modules/profiles/components/user-profile-page/user-profile-page.component';
 import { UserProfileInfoComponent } from './modules/profiles/components/user-profile-info/user-profile-info.component';
+import { NotFoundPageComponent } from './shared/not-found-page/not-found-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'profiles/:id', component: UserProfilePageComponent, children: [
     { path: 'info', component: UserProfileInfoComponent  },
-  ] }
+  ] },
+  { path: '**', component: NotFoundPageComponent }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { HomePageComponent } from './shared/home-page/home-page.component';
 import { AlertListComponent } from './shared/components/alert-list/alert-list.component';
 import { ProfilesModule } from './modules/profiles/profiles.module';
+import { NotFoundPageComponent } from './shared/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ProfilesModule } from './modules/profiles/profiles.module';
     HeaderComponent,
     HomePageComponent,
     AlertListComponent,
+    NotFoundPageComponent,
   ],
   imports: [
     BrowserModule,
