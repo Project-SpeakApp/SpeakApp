95b71abb6d77dea99f32851ab9a2b83808eb1448
Karol <socer404@gmail.com>
2023-12-11 23:07:15 +0100

    Setup project for eureka discovery service

---
+++ backend/discovery-service/src/main/java/com/speakapp/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,8 @@
+package com.speakapp.discoveryservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+}
