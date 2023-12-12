82c59b1eb4291a45deed49d749064caf45d78a52
macius02 <mkuzmicki2002@gmail.com>
2023-12-12 13:35:59 +0100

    Deleted unwanted line of codes

--- frontend/src/app/modules/posts/sevices/post.service.ts
+++ frontend/src/app/modules/posts/sevices/post.service.ts
@@ -18,8 +18,6 @@ export class PostService {
   addPost(model: AddPost, userId: string): Observable<void> {
     this.isLoading.set(true);
     const headers = new HttpHeaders().set('UserId', userId);
-    new Promise(resolve => setTimeout(resolve, 5000));
-
     return this.http.post<void>('http://localhost:8082/api/posts', model, {headers}).pipe(
       finalize( () => {
         this.isLoading.set(false);
