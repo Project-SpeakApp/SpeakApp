3aa587209d139271745b7088ef834a77f98448a7
Karol <socer404@gmail.com>
2023-12-12 19:07:19 +0100

    Api-gateway is running properly local but doesn't with docker

--- backend/post-service/src/main/java/com/speakapp/postservice/PostServiceApplication.java
+++ backend/post-service/src/main/java/com/speakapp/postservice/PostServiceApplication.java
@@ -2,10 +2,10 @@ package com.speakapp.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient // This annotation enables the Eureka Client in our application.
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
