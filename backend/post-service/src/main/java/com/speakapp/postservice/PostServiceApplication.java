87f51dbd46aac418edc25c227dbc6b8bc5cccf9e
Karol <socer404@gmail.com>
2023-12-12 17:24:27 +0100

    Add Eureka Client to user-service and post-service

--- backend/post-service/src/main/java/com/speakapp/postservice/PostServiceApplication.java
+++ backend/post-service/src/main/java/com/speakapp/postservice/PostServiceApplication.java
@@ -2,8 +2,10 @@ package com.speakapp.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
