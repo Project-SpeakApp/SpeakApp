5105a868445e44ee8bf85b87804d06e4277ee3f4
Krzysztof <krzysztof.andrzej.jurkowski@gmail.com>
2024-01-06 21:27:19 +0100

    reaction service

--- frontend/src/app/modules/posts/components/post-bottom-bar/post-bottom-bar.component.ts
+++ frontend/src/app/modules/posts/components/post-bottom-bar/post-bottom-bar.component.ts
@@ -1,20 +1,33 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {PostGet} from "../../../../shared/types/posts/post-get.model";
 import {DateFormatting} from "../../../../shared/util/DateFormatting";
+import { ReactionType } from 'src/app/shared/types/posts/ReactionType.enum';
+import { ReactionService } from '../../sevices/reaction.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-bottom-bar',
   templateUrl: './post-bottom-bar.component.html',
   styleUrls: ['./post-bottom-bar.css']
 })
-export class PostBottomBarComponent implements OnInit{
-  ngOnInit(): void {
-    this.checkIfPostWasEdited();
-  }
+export class PostBottomBarComponent implements OnInit, OnDestroy {
   @Input() post: PostGet = {} as PostGet;
 
   formattedDate: string = '';
   isVisible: boolean = true;
+
+  subscription: Subscription = new Subscription();
+
+  // musze tak bo nie działają enumy w htmlu
+  like = ReactionType.LIKE;
+  love = ReactionType.LOVE;
+  haha = ReactionType.HA_HA;
+  wow = ReactionType.WOW;
+  sad = ReactionType.SAD;
+  wrr = ReactionType.WRR;
+
+  constructor(private reactionService: ReactionService) { }
+
 
   checkIfPostWasEdited() {
     if(this.post.modifiedAt == null || (this.post.createdAt.valueOf() == this.post.modifiedAt.valueOf())) {
@@ -24,5 +37,19 @@ export class PostBottomBarComponent implements OnInit{
       this.isVisible = true;
       this.formattedDate = DateFormatting.formatDateTime(this.post.modifiedAt);
     }
+  }
+
+  upsertReaction(reactionType: ReactionType) {
+    if (this.reactionService.isLoading()) return;
+    this.reactionService.upsertReactionToPost(this.post.postId, reactionType, this.post.currentUserReaction)
+      .subscribe();
+  }
+
+  ngOnInit(): void {
+    this.checkIfPostWasEdited();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- frontend/src/app/modules/posts/sevices/post.service.ts
+++ frontend/src/app/modules/posts/sevices/post.service.ts
@@ -80,7 +80,4 @@ export class PostService {
 
     );
   }
-
-
-
 }
