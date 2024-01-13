0e271f2127534ee1c89494acd25c48bcbe1cc803
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2024-01-13 16:51:23 +0100

    Make GlobalExcpetionHandler extend from basic Spring MVC exception handler

--- backend/post-service/src/main/java/com/speakapp/postservice/advice/GlobalExceptionHandler.java
+++ backend/post-service/src/main/java/com/speakapp/postservice/advice/GlobalExceptionHandler.java
@@ -12,9 +12,10 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(value = MethodArgumentNotValidException.class)
   public ResponseEntity<ApiError> handleValidationError(MethodArgumentNotValidException ex){
