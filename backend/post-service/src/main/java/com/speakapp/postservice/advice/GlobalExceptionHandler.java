4c7636833172caa6fa528240a84de653c64b0147
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2024-01-14 03:38:32 +0100

    Refactor

--- backend/post-service/src/main/java/com/speakapp/postservice/advice/GlobalExceptionHandler.java
+++ backend/post-service/src/main/java/com/speakapp/postservice/advice/GlobalExceptionHandler.java
@@ -1,56 +1,53 @@
 package com.speakapp.postservice.advice;
 
-import com.speakapp.postservice.exceptions.AccessDeniedException;
 import com.speakapp.postservice.exceptions.ServiceLayerException;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
+@RestControllerAdvice
+public class GlobalExceptionHandler{
+  // change existing handling method
   @ExceptionHandler(value = MethodArgumentNotValidException.class)
-  public ResponseEntity<ApiError> handleValidationError(MethodArgumentNotValidException ex){
-    String errorMessage = "Data validation error";
-    return new ResponseEntity<>(new ApiError(ex, buildValidationErrorsMap(ex, errorMessage)), HttpStatus.BAD_REQUEST);
+  public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex){
+    String mainErrorMessage = "Data validation error";
+    return new ResponseEntity<>(new ApiError(ex, buildValidationErrorMessagesArray(ex.getFieldErrors(), mainErrorMessage)), HttpStatus.BAD_REQUEST);
   }
-
+  // create new handle method
   @ExceptionHandler(value = ServiceLayerException.class)
-  public ResponseEntity<ApiError> handleServiceLayerError(AccessDeniedException ex){
-    return new ResponseEntity<>(new ApiError(ex, buildErrorsMap(ex)), HttpStatus.FORBIDDEN);
+  public ResponseEntity<ApiError> handleServiceLayerError(ServiceLayerException ex){
+    return new ResponseEntity<>(new ApiError(ex, buildErrorMessagesArray(ex.getMessage())), HttpStatus.FORBIDDEN);
   }
 
-  private List<String> buildErrorsMap(Exception ex){
-    List<String> errorsList = new ArrayList<>();
-    errorsList.add(ex.getMessage());
-    return errorsList;
+  // add default handling for any other exception
+  @ExceptionHandler(value = Exception.class)
+  public ResponseEntity<ApiError> handleUnexpectedException(Exception ex){
+    String mainErrorMessage = "Unexpected error";
+    return new ResponseEntity<>(new ApiError(ex, buildErrorMessagesArray(mainErrorMessage)), HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+  private String [] buildErrorMessagesArray(String mainErrorMessage){
+    List<String> errorMessagesList = new ArrayList<>();
+    errorMessagesList.add(mainErrorMessage);
+    return errorMessagesList.toArray(new String[0]);
   }
 
-  private List<String> buildValidationErrorsMap(MethodArgumentNotValidException ex, String errorMessage){
-    List<String> errorsList = new ArrayList<>();
-    errorsList.add(errorMessage);
-
-    List<FieldError> fieldErrors = ex.getFieldErrors();
+  private String [] buildValidationErrorMessagesArray(List<FieldError> fieldErrors, String mainErrorMessage){
+    List<String> errorMessagesList = new ArrayList<>();
+    errorMessagesList.add(mainErrorMessage);
 
     for(FieldError fieldError : fieldErrors){
-      errorsList.add("Error in field "
-          + fieldError.getField() +
-          ", rejected value "
-          + fieldError.getRejectedValue() +
-          ", "
+      errorMessagesList.add("Error in field ["
+          + fieldError.getField()
+          + "] - "
           + fieldError.getDefaultMessage());
     }
 
-    return errorsList;
+    return errorMessagesList.toArray(new String[0]);
   }
 
 }
