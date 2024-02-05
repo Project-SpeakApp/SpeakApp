108e6054c7ccb4f44b133e99c5386b9dbd33c880
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2024-02-05 19:25:37 +0100

    api routes quick fix

--- frontend/src/app/modules/posts/sevices/comment.service.ts
+++ frontend/src/app/modules/posts/sevices/comment.service.ts
@@ -1,8 +1,7 @@
-import {Injectable, signal} from '@angular/core';
-import {BehaviorSubject, finalize, Observable, tap} from "rxjs";
+import {Injectable} from '@angular/core';
+import {finalize, Observable, tap} from "rxjs";
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {AlertService} from "../../../shared/services/alert.service";
-import {CommentGetModel} from "../../../shared/types/posts/comment-get.model";
 import {CommentGetListModel} from "../../../shared/types/posts/comment-get-list.model";
 
 @Injectable({
@@ -14,7 +13,7 @@ export class CommentService {
   getComments(postId: string, userId: string, currentPage: number, pageSize: number): Observable<CommentGetListModel> {
     const headers = new HttpHeaders().set('UserId', userId);
     let params = new HttpParams().set('postId', postId).set('pageNumber', currentPage).set('pageSize', pageSize);
-    return this.http.get<CommentGetListModel>('http://localhost:8082/api/comments', {headers, params}).pipe( //tez nie jestem pewny czy tak powinien wygladac endpoint
+    return this.http.get<CommentGetListModel>('http://localhost:8080/api/comments', {headers, params}).pipe( //tez nie jestem pewny czy tak powinien wygladac endpoint
       finalize( () => {
       }),
       tap (
