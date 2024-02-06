f5cf8d9b778078cf0843987492f0ccfa72120a1e
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2024-02-06 16:09:24 +0100

    Add exception handler and exceptions

---
+++ backend/user-service/src/main/java/com/speakapp/userservice/exception_handler/GlobalExceptionHandler.java
@@ -0,0 +1,61 @@
+package com.speakapp.userservice.exception_handler;
+
+import com.speakapp.springexceptionhandler.ServiceLayerException;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(value = MethodArgumentNotValidException.class)
+  public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    String mainErrorMessage = "Data validation error";
+
+    return new ResponseEntity<>(
+        new ApiError(ex, buildValidationErrorMessagesArray(ex.getFieldErrors(), mainErrorMessage)),
+        HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(value = ServiceLayerException.class)
+  public ResponseEntity<ApiError> handleServiceLayerExceptionException(ServiceLayerException ex) {
+    return new ResponseEntity<>(new ApiError(ex, buildErrorMessagesArray(ex.getMessage())),
+        ex.getHttpStatus());
+  }
+
+  @ExceptionHandler(value = Exception.class)
+  public ResponseEntity<ApiError> handleUnexpectedException(Exception ex) {
+    String mainErrorMessage = "Unexpected error";
+
+    return new ResponseEntity<>(new ApiError(ex, buildErrorMessagesArray(mainErrorMessage)),
+        HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  private String[] buildErrorMessagesArray(String mainErrorMessage) {
+    List<String> errorMessagesList = new ArrayList<>();
+    errorMessagesList.add(mainErrorMessage);
+
+    return errorMessagesList.toArray(new String[0]);
+  }
+
+  private String[] buildValidationErrorMessagesArray(List<FieldError> fieldErrors,
+      String mainErrorMessage) {
+    List<String> errorMessagesList = new ArrayList<>();
+    errorMessagesList.add(mainErrorMessage);
+
+    for (FieldError fieldError : fieldErrors) {
+      errorMessagesList.add("Error in field ["
+          + fieldError.getField()
+          + "] - "
+          + fieldError.getDefaultMessage());
+    }
+
+    return errorMessagesList.toArray(new String[0]);
+  }
+
+}
