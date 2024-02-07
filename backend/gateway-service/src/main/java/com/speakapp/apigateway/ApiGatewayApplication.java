9b9f932585de22c6ac60a108b401faa15053acfe
szlazakm <01169634@pw.edu.pl>
2024-02-07 16:13:02 +0100

    removed auth-service

--- backend/gateway-service/src/main/java/com/speakapp/apigateway/ApiGatewayApplication.java
+++ backend/gateway-service/src/main/java/com/speakapp/apigateway/ApiGatewayApplication.java
@@ -22,10 +22,6 @@ public class ApiGatewayApplication {
                         .path("/api/users/**")
                         .uri("http://user-service:8081")
                 )
-                .route("auth-service", r -> r
-                        .path("/api/auth/**")
-                        .uri("http://auth-service:8083")
-                )
                 .build();
     }
 }
