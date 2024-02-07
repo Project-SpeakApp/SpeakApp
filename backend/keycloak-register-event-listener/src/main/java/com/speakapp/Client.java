aab24740e93a7dd9fd98c15a621d50c690d111f0
szlazakm <01169634@pw.edu.pl>
2024-02-07 16:07:33 +0100

    connection between keycloak and api gateway

--- backend/keycloak-register-event-listener/src/main/java/com/speakapp/Client.java
+++ backend/keycloak-register-event-listener/src/main/java/com/speakapp/Client.java
@@ -12,41 +12,37 @@ import java.util.concurrent.CompletableFuture;
 
 public class Client {
 
+    private Client(){}
     private static final HttpClient httpClient = HttpClient.newBuilder().build();
-    private static final Logger log = Logger.getLogger(CustomEventListenerProvider.class);
+    private static final Logger log = Logger.getLogger(Client.class);
 
     public static void postService(AppUserDTO appUserDTO) {
         try {
 
             String requestBody = appUserToBody(appUserDTO);
-            log.info(requestBody);
-            // Create the HTTP request
+
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI("http://localhost:8080/api/users"))
+                    .uri(new URI("http://api-gateway:8080/api/users"))
                     .header("Content-Type", "application/json")
                     .POST(HttpRequest.BodyPublishers.ofString(requestBody))
                     .build();
 
             CompletableFuture<HttpResponse<String>> responseFuture = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
 
-            // Handle the response
             responseFuture.thenAccept(response -> {
-                // Get the response status code
                 int statusCode = response.statusCode();
-                System.out.println("Response Code: " + statusCode);
-
-                // Get the response body
                 String responseBody = response.body();
-                System.out.println("Response: " + responseBody);
-            }).join(); // Wait for the response
+                log.infof("Status code: %d\nResponse body: %s\n", statusCode, responseBody);
+            }).join();
 
         } catch (URISyntaxException e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
     }
 
     private static String appUserToBody(AppUserDTO appUserDTO) {
-        return "{\"firstName\":\"" + appUserDTO.getFirstName() +
+        return "{\"userId\":\"" + appUserDTO.getUserId() +
+                "\", \"firstName\":\"" + appUserDTO.getFirstName() +
                 "\", \"lastName\":\"" + appUserDTO.getLastName() +
                 "\", \"email\":\"" + appUserDTO.getEmail() +
                 "\", \"dateOfBirth\":\"" + appUserDTO.getDateOfBirth() + "\"}";
