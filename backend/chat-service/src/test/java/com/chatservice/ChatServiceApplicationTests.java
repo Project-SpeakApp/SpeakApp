d7f98c76929eddf47f71273789f58645207df1f0
Maciej Kwiatkowski <maciek-kwiatkowski@o2.pl>
2024-03-01 19:09:40 +0100

    sec commit

---
+++ backend/chat-service/src/test/java/com/chatservice/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
