c816ef910abb23ae4eec0049017efd1f36197150
Karol Sekscinski <socer404@gmail.com>
2024-03-16 16:48:08 +0100

    Blob Storage Service dependencies and basics

---
+++ backend/blob-service/src/main/java/com/speakapp/blobservice/AzureBlobStorageApplication.java
@@ -0,0 +1,12 @@
+package com.speakapp.blobservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AzureBlobStorageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AzureBlobStorageApplication.class, args);
+    }
+}
