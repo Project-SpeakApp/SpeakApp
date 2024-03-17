4ff34b6533657e3a52905959e285eeb1e73ef0c3
Karol Sekscinski <socer404@gmail.com>
2024-03-17 16:02:28 +0100

    Delete files (from testing stage)

--- backend/blob-service/src/main/java/com/speakapp/blobservice/exception/AzureBlobStorageException.java
+++
@@ -1,8 +0,0 @@
-package com.speakapp.blobservice.exception;
-
-public class AzureBlobStorageException extends Exception {
-    public AzureBlobStorageException(String message) {
-        super(message);
-    }
-
-}
