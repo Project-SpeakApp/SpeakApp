bbed5df969f3b1b494a294d1fc94926d52751ef9
Karol Sekscinski <socer404@gmail.com>
2024-03-17 23:01:14 +0100

    Finish configuration for Azure Blob Storage and PostgreSQL

--- backend/blob-service/src/main/java/com/speakapp/blobservice/configurations/AzureBlobStorageConfiguration.java
+++ backend/blob-service/src/main/java/com/speakapp/blobservice/configurations/AzureBlobStorageConfiguration.java
@@ -1,8 +1,10 @@
-package com.speakapp.blobservice.configuration;
+package com.speakapp.blobservice.configurations;
 
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.speakapp.blobservice.mappers.BlobMapper;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- backend/blob-service/src/main/java/com/speakapp/blobservice/exceptions/AzureBlobStorageException.java
+++ backend/blob-service/src/main/java/com/speakapp/blobservice/exceptions/AzureBlobStorageException.java
@@ -1,4 +1,4 @@
-package com.speakapp.blobservice.exception;
+package com.speakapp.blobservice.exceptions;
 
 public class AzureBlobStorageException extends Exception {
     public AzureBlobStorageException(String message) {
