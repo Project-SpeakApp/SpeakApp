9e48d8347cbb6110558edde103a586479d3627a6
Karol Sekscinski <socer404@gmail.com>
2024-03-19 00:49:53 +0100

    Delete old exception

--- backend/blob-service/src/main/java/com/speakapp/blobservice/exceptions/AzureBlobStorageException.java
+++
@@ -1,8 +0,0 @@
-package com.speakapp.blobservice.exceptions;
-
-public class AzureBlobStorageException extends Exception {
-    public AzureBlobStorageException(String message) {
-        super(message);
-    }
-
-}
