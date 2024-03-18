30da17086f47be185ef71c1ac882f8fd6f462ec5
Karol Sekscinski <socer404@gmail.com>
2024-03-19 00:34:41 +0100

    Add DiscoveryClient to Blob-Service

--- backend/blob-service/src/main/java/com/speakapp/blobservice/AzureBlobStorageApplication.java
+++ backend/blob-service/src/main/java/com/speakapp/blobservice/AzureBlobStorageApplication.java
@@ -2,9 +2,11 @@ package com.speakapp.blobservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AzureBlobStorageApplication {
     public static void main(String[] args) {
         SpringApplication.run(AzureBlobStorageApplication.class, args);
