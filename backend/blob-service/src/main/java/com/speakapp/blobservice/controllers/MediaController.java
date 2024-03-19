56c0d6579b06bee8b9dde4aafe96e43b5629bfbe
Karol Sekscinski <socer404@gmail.com>
2024-03-19 01:20:58 +0100

    Add temporary CRUD operations like delete or put

--- backend/blob-service/src/main/java/com/speakapp/blobservice/controllers/MediaController.java
+++ backend/blob-service/src/main/java/com/speakapp/blobservice/controllers/MediaController.java
@@ -4,6 +4,8 @@ import com.speakapp.blobservice.dtos.BlobFileDTO;
 import com.speakapp.blobservice.dtos.BlobMetadataDTO;
 import com.speakapp.blobservice.entities.TypeMedia;
 import com.speakapp.blobservice.services.MediaService;
+import com.speakapp.blobservice.utils.JwtDecoder;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,43 +19,47 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/media")
+@SecurityRequirement(name = "bearerAuth")
 @RequiredArgsConstructor
 public class MediaController {
 
         private final MediaService mediaService;
+        private final JwtDecoder jwtDecoder;
+        private static final String AUTH_HEADER_PREFIX = "Bearer ";
         @GetMapping("/{mediaId}")
         @ResponseStatus(HttpStatus.OK)
         public ResponseEntity<byte[]> downloadMedia(@PathVariable UUID mediaId) {
             BlobFileDTO file = mediaService.downloadMediaByUUID(mediaId);
             return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).body(file.getContent());
         }
-        @PostMapping("/upload")
+        @PostMapping
         @ResponseStatus(HttpStatus.CREATED)
         public ResponseEntity<String> uploadMedia(@RequestParam TypeMedia type,
                                                   @RequestBody MultipartFile file,
-                                                  @RequestHeader("UserId") UUID userId) throws IOException {
-            if (file.isEmpty()) {
-
-                return new ResponseEntity<>("No file has been uploaded", HttpStatus.BAD_REQUEST);
-            }
-            if (!isValidFileFormat(file.getOriginalFilename())) {
-                return new ResponseEntity<>("Invalid file format Only JPEG, JPG, PNG files are allowed", HttpStatus.BAD_REQUEST);
-
-            }
+                                                  @RequestHeader("Authorization") String authHeader) throws IOException {
+            String jwtToken = authHeader.replace(AUTH_HEADER_PREFIX, "");
+            UUID userId = jwtDecoder.extractUserIdFromJwt(jwtToken);
             BlobMetadataDTO blobMetadataDTO = mediaService.uploadMedia(file, type, userId);
-            String mediaId = blobMetadataDTO.getMediaId();
-            return ResponseEntity.ok(mediaId);
-
+            return ResponseEntity.ok(blobMetadataDTO.getMediaId());
         }
 
-        private boolean isValidFileFormat(String fileName) {
-            String[] validFileFormats = {"jpeg", "jpg", "png"};
-            String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1);
-            for (String validFileFormat : validFileFormats) {
-                if (fileExtension.equalsIgnoreCase(validFileFormat)) {
-                    return true;
-                }
-            }
-            return false;
+        @DeleteMapping("/{mediaId}")
+        @ResponseStatus(HttpStatus.NO_CONTENT)
+        public void deleteMedia(@PathVariable UUID mediaId, @RequestHeader("Authorization") String authHeader) {
+            String jwtToken = authHeader.replace(AUTH_HEADER_PREFIX, "");
+            UUID userId = jwtDecoder.extractUserIdFromJwt(jwtToken);
+            mediaService.deleteMedia(userId, mediaId);
         }
+
+        @PutMapping("/{mediaId}")
+        @ResponseStatus(HttpStatus.OK)
+        public ResponseEntity<String> updateMedia(@PathVariable UUID mediaId,
+                                                  @RequestBody MultipartFile file,
+                                                  @RequestHeader("Authorization") String authHeader) throws IOException {
+            String jwtToken = authHeader.replace(AUTH_HEADER_PREFIX, "");
+            UUID userId = jwtDecoder.extractUserIdFromJwt(jwtToken);
+            BlobMetadataDTO blobMetadataDTO = mediaService.updateMedia(file, mediaId, userId);
+            return ResponseEntity.ok(blobMetadataDTO.getMediaId());
+        }
+
 }
