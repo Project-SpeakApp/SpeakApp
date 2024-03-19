7906c82cc0e61e497aa53759ebaf97871c671355
Karol Sekscinski <socer404@gmail.com>
2024-03-19 01:22:51 +0100

    Delete tests which cannot be executed without connection to db and blob storage

--- backend/blob-service/src/test/java/com/speakapp/blobservice/AzureBlobStorageApplicationTests.java
+++ backend/blob-service/src/test/java/com/speakapp/blobservice/AzureBlobStorageApplicationTests.java
@@ -18,45 +18,4 @@ class AzureBlobStorageApplicationTests {
     @Test
     void contextLoads() {
     }
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Test
-    void whenFileUploaded_thenVerifyStatus()
-            throws Exception {
-        MockMultipartFile file
-                = new MockMultipartFile(
-                "file",
-                "hello.txt",
-                MediaType.TEXT_PLAIN_VALUE,
-                "Hello, World!".getBytes()
-        );
-
-        MockMvc mockMvc
-                = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        mockMvc.perform(multipart("/api/media/upload")
-                        .file(file)
-                        .param("type", "IMAGE")
-                        .header("UserId", "6c84fb95-12c4-11ec-82a8-0242ac130003"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void whenEmptyFileUploaded_thenVerifyStatusNotFound() throws Exception {
-        MockMultipartFile file = new MockMultipartFile(
-                "file",
-                "",
-                MediaType.TEXT_PLAIN_VALUE,
-                "".getBytes()
-        );
-
-        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        mockMvc.perform(multipart("/api/media/upload")
-                        .file(file)
-                        .param("type", "AVATAR")  // Add type parameter
-                        .header("UserId", "6c84fb95-12c4-11ec-82a8-0242ac130003")) // Add UserId header
-                .andExpect(status().isNotFound());
-    }
-
-
 }
