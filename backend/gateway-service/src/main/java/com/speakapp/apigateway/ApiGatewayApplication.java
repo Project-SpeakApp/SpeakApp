88a2e7e6d43208a3c1caaf400809182a8c159bfb
Karol Sekscinski <socer404@gmail.com>
2024-04-20 04:54:33 +0200

    Load balancer and dynamic routing

--- backend/gateway-service/src/main/java/com/speakapp/apigateway/ApiGatewayApplication.java
+++ backend/gateway-service/src/main/java/com/speakapp/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package com.speakapp.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,17 +15,18 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
     @Bean
+    @LoadBalanced
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("post-service", r -> r
                         .path("/api/posts/**", "/api/comments/**")
-                        .uri("http://post-service:8082"))
+                        .uri("lb://post-service")) //post-service:8082
                 .route("user-service", r -> r
                         .path("/api/users/**")
-                        .uri("http://user-service:8081"))
+                        .uri("lb://user-service")) //user-service:8081
                 .route("media-service", r -> r
                         .path("/api/media/**")
-                        .uri("http://blob-service:8083")
+                        .uri("lb://blob-service") //blob-service:8083
                 )
                 .build();
     }
