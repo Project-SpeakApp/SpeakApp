0fc3fdd79f65da84d349d358d94ec42a78d932ef
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2024-05-02 22:15:08 +0200

    Change query condition to equals and add query for searching only by name

--- backend/user-service/src/main/java/com/speakapp/userservice/repositories/UserRepository.java
+++ backend/user-service/src/main/java/com/speakapp/userservice/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.speakapp.userservice.repositories;
 
 import com.speakapp.userservice.entities.AppUser;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,7 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, UUID> {
 
-  Page<AppUser> findAllByFirstNameStartingWithAndLastNameStartingWith(String firstName, String lastName);
+  Page<AppUser> findAllByFirstNameEqualsAndLastNameEquals(String firstName, String lastName, Pageable page);
+  Page<AppUser> findAllByFirstNameEquals(String firstName, Pageable page);
+
 }
