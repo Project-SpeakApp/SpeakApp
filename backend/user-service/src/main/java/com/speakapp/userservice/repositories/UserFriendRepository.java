68b4f7d8984129d10848837a680978962240702e
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2024-05-04 02:04:23 +0200

    add basic functionality on friends requests (send, accept, reject, get page of previews)

---
+++ backend/user-service/src/main/java/com/speakapp/userservice/repositories/UserFriendRepository.java
@@ -0,0 +1,32 @@
+package com.speakapp.userservice.repositories;
+
+import com.speakapp.userservice.entities.AppUser;
+import com.speakapp.userservice.entities.FriendStatus;
+import com.speakapp.userservice.entities.UserFriend;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserFriendRepository extends JpaRepository<UserFriend, UUID> {
+
+    boolean existsByRequesterAndAddressee(AppUser requester,
+                                          AppUser addressee);
+
+    Optional<UserFriend> findByIdAndAddresseeAndStatus(UUID id,
+                                                       AppUser addressee,
+                                                       FriendStatus status);
+
+    void deleteByIdAndAddresseeAndStatus(UUID id,
+                                         AppUser addressee,
+                                         FriendStatus status);
+
+    Page<UserFriend> findAllByAddresseeAndStatusOrderByCreatedAtDesc(AppUser addressee,
+                                                                     FriendStatus status,
+                                                                     Pageable pageable);
+
+}
