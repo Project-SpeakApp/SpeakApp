c71b62a9096b8222512d0b7054cd04d645adc92a
szlazakm <01169634@pw.edu.pl>
2024-05-09 17:57:58 +0200

    Deleted potential circular dependency

--- backend/post-service/src/main/java/com/speakapp/postservice/services/FavouriteListService.java
+++ backend/post-service/src/main/java/com/speakapp/postservice/services/FavouriteListService.java
@@ -1,14 +1,18 @@
 package com.speakapp.postservice.services;
 
-import com.speakapp.postservice.dtos.FavouriteListAddPostDTO;
-import com.speakapp.postservice.dtos.FavouriteListCreateDTO;
-import com.speakapp.postservice.dtos.FavouriteListDeletePostDTO;
-import com.speakapp.postservice.dtos.PostPageGetDTO;
+import com.speakapp.postservice.communication.UserServiceCommunicationClient;
+import com.speakapp.postservice.dtos.*;
 import com.speakapp.postservice.entities.FavouriteList;
 import com.speakapp.postservice.entities.Post;
+import com.speakapp.postservice.entities.PostReaction;
+import com.speakapp.postservice.entities.ReactionType;
 import com.speakapp.postservice.exceptions.FavouriteListNotFoundException;
 import com.speakapp.postservice.exceptions.PostNotFoundException;
+import com.speakapp.postservice.mappers.PostMapper;
+import com.speakapp.postservice.mappers.PostPageMapper;
+import com.speakapp.postservice.repositories.CommentRepository;
 import com.speakapp.postservice.repositories.FavouriteListRepository;
+import com.speakapp.postservice.repositories.PostReactionRepository;
 import com.speakapp.postservice.repositories.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -16,8 +20,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +28,11 @@ public class FavouriteListService {
 
     private final FavouriteListRepository favouriteListRepository;
     private final PostRepository postRepository;
-    private final PostService postService;
+    private final CommentRepository commentRepository;
+    private final PostReactionRepository postReactionRepository;
+    private final PostMapper postMapper;
+    private final PostPageMapper postPageMapper;
+    private final UserServiceCommunicationClient userServiceCommunicationClient;
 
     public void createFavouriteList(FavouriteListCreateDTO favouriteListCreateDTO) {
         FavouriteList newFavouriteList = FavouriteList.builder()
@@ -42,7 +49,7 @@ public class FavouriteListService {
         );
 
         Page<Post> favouritePosts = postRepository.findByFavouriteListsOrderByCreatedAtDesc(favouriteList, page);
-        return postService.createPostPageGetDTOFromPostPage(favouritePosts, userId, page);
+        return createPostPageGetDTOFromPostPage(favouritePosts, userId, page);
     }
 
     public void addPost(UUID userId, FavouriteListAddPostDTO favouriteListAddPostDTO) {
@@ -76,5 +83,59 @@ public class FavouriteListService {
         );
 
         favouriteListRepository.save(favouriteList);
+    }
+
+    private PostPageGetDTO createPostPageGetDTOFromPostPage(Page<Post> postsPage, UUID userId, Pageable page) {
+        List<PostGetDTO> postGetDTOS = postsPage.getContent().stream().map(post -> {
+            UserGetDTO postAuthor = userServiceCommunicationClient.getUserById(post.getUserId());
+            ReactionsGetDTO postReactions = getReactionsForThePost(post);
+            ReactionType currentUserReactionType = postReactionRepository.findTypeByPostAndUserId(post, userId).orElse(null);
+            Long totalNumberOfComments = commentRepository.countAllByPost(post);
+            boolean favourite = isPostFavourite(userId, post);
+
+            return postMapper.toGetDTO(
+                    post,
+                    postAuthor,
+                    postReactions,
+                    currentUserReactionType,
+                    totalNumberOfComments,
+                    favourite
+            );
+        }).toList();
+
+        return postPageMapper.toGetDTO(
+                postGetDTOS,
+                page,
+                postsPage.getTotalPages()
+        );
+    }
+
+    private ReactionsGetDTO getReactionsForThePost(Post post) {
+        List<PostReaction> postReactions = postReactionRepository.findAllByPost(post);
+
+        Map<ReactionType, Long> sumOfReactionsByType = new EnumMap<>(ReactionType.class);
+        for (PostReaction postReaction : postReactions) {
+            sumOfReactionsByType.merge(postReaction.getType(), 1L, Long::sum);
+        }
+
+        return ReactionsGetDTO.builder()
+                .sumOfReactionsByType(sumOfReactionsByType)
+                .sumOfReactions(sumOfReactionsByType
+                        .values()
+                        .stream()
+                        .reduce(0L, Long::sum)
+                )
+                .build();
+    }
+
+    private boolean isPostFavourite(UUID userId, Post post) {
+
+        Optional<FavouriteList> favouriteList = favouriteListRepository.getFavouriteListByUserId(userId);
+
+        if(favouriteList.isEmpty()) {       //If it doesn't exist
+            return false;
+        }
+
+        return favouriteList.get().getFavouritePosts().contains(post);
     }
 }
--- backend/post-service/src/main/java/com/speakapp/postservice/services/PostService.java
+++ backend/post-service/src/main/java/com/speakapp/postservice/services/PostService.java
@@ -131,7 +131,7 @@ public class PostService {
                 .build();
     }
 
-    protected PostPageGetDTO createPostPageGetDTOFromPostPage(Page<Post> postsPage, UUID userId, Pageable page) {
+    private PostPageGetDTO createPostPageGetDTOFromPostPage(Page<Post> postsPage, UUID userId, Pageable page) {
         List<PostGetDTO> postGetDTOS = postsPage.getContent().stream().map(post -> {
             UserGetDTO postAuthor = userServiceCommunicationClient.getUserById(post.getUserId());
             ReactionsGetDTO postReactions = getReactionsForThePost(post);
