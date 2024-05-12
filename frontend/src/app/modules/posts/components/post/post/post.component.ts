f9bb5678dd1af962dbff2aa0e367640f9ba3db7c
Krzysztof <krzysztof.andrzej.jurkowski@gmail.com>
2024-05-12 13:42:55 +0200

    fix edited on deleting image

--- frontend/src/app/modules/posts/components/post/post/post.component.ts
+++ frontend/src/app/modules/posts/components/post/post/post.component.ts
@@ -105,7 +105,9 @@ export class PostComponent implements OnChanges, OnInit, OnDestroy {
     this.post.mediaId = null;
     this.imageUrl = null;
     this.imageSub.add(this.postService.updatePost(this.post.postId, {content: this.post.content, mediaId: null}).subscribe(
-      (updatedPost) => {this.contentUpdated.emit(updatedPost);}
+      (updatedPost) => {
+        this.post = updatedPost;
+      }
     ));
   }
 
